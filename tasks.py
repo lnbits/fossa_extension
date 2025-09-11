e5eeb337a42f04d367c0b0d40d2c30e59a9a7b03
Arc <ben@arc.wales>
2025-09-11 06:02:05 +0100

    use api call instead of import

--- tasks.py
+++ tasks.py
@@ -1,11 +1,13 @@
 import asyncio
 
+import httpx
+from lnbits.core.crud import get_wallet
 from lnbits.core.models import Payment
 from lnbits.core.services import websocket_updater
+from lnbits.settings import settings
 from lnbits.tasks import register_invoice_listener
-from loguru import logger
 
-from .crud import get_fossa_payment_by_hash, update_fossa_payment
+from .crud import get_fossa, get_fossa_payment_by_hash, update_fossa_payment
 
 
 async def wait_for_paid_invoices():
@@ -24,19 +26,23 @@ async def on_invoice_paid(payment: Payment) -> None:
 
     swap_id = payment.extra.get("swap_id")
     if swap_id:
+        fossa_payment = await get_fossa_payment_by_hash("pending_swap_" + swap_id)
+        if not fossa_payment:
+            return
+        fossa = await get_fossa(fossa_payment.fossa_id)
+        if not fossa:
+            return
+        wallet = await get_wallet(fossa.wallet)
+        if not wallet:
+            return
         try:
-            from ..boltz.views_api import api_swap_status
-
-            swap = await api_swap_status(swap_id)
-            if swap:
-                fossa_payment = await get_fossa_payment_by_hash(
-                    "pending_swap_" + swap_id
+            async with httpx.AsyncClient() as client:
+                swap = await client.post(
+                    url=f"http://{settings.host}:{settings.port}/boltz/api/v1/swap/status",
+                    headers={"X-API-KEY": wallet.adminkey},
+                    json={"swapId": swap_id},
                 )
-                if not fossa_payment:
-                    logger.error(
-                        f"Boltz: could not find fossa payment for swap {swap_id}"
-                    )
-                    return
+            if swap:
                 fossa_payment.payment_hash = swap_id
                 await update_fossa_payment(fossa_payment)
                 await websocket_updater("pending_swap_" + swap_id, "Paid")
